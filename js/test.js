1420cb2ba4dae6a025a89c5e82e917e1b96f476f
Pooja Kumbhar <pooja.p.kumbhar@gmail.com>
2022-09-28 21:00:18 +0200

    Add code for creating window object

--- js/test.js
+++ js/test.js
@@ -6,19 +6,42 @@ names[1]="John";
 names[2]="Jen";
 names[3]="Jason";
 names[4]="Paul";
-names[4]="Frank";
-names[4]="Larry";
-names[4]="Paula";
-names[4]="Laura";
-names[4]="jim";
+names[5]="Frank";
+names[6]="Larry";
+names[7]="Paula";
+names[8]="Laura";
+names[9]="jim";
 
-for(var i=0; i<names.length; i++)
+
+
+
+(function(window){
+    var byeSpeaker={};
+  byeSpeaker.goodbye=function() {
+    for(var i=0; i<names.length; i++)
 {
     if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j')
     {
 console.log("Goodbye "+names[i]);
-    }else
+    } 
+}}
+window.byeSpeaker=byeSpeaker;
+})(window);
+
+
+(function(window){
+    var helloSpeaker={};
+  helloSpeaker.hello=function() {
+    for(var i=0; i<names.length; i++)
+{
+    if(names[i].charAt(0)!=='J' && names[i].charAt(0)!=='j')
     {
 console.log("Hello "+names[i]);
-    }
-}
+    } 
+}}
+window.helloSpeaker=helloSpeaker;
+})(window);
+
+
+helloSpeaker.hello();
+byeSpeaker.goodbye();
